45095566bc57fbf15d90e3e5560262c865eb1189
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-03-27 17:14:54 -0300

    test: create category use case

--- src/app/use-cases/categories/create-category-use-case.ts
+++ src/app/use-cases/categories/create-category-use-case.ts
@@ -1,20 +1,10 @@
 import { ICreateCategoryDTO } from '../../dtos/create-category-dto';
 import { ICategoriesRepository } from '../../repositories/categories-repository';
 
-import { AppError } from '@/app/errors/app-error';
-
 export class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   async execute({ name, emoji }: ICreateCategoryDTO) {
-    if (!name) {
-      throw new AppError('Name is required.', 400);
-    }
-
-    if (!emoji) {
-      throw new AppError('Emoji is required.', 400);
-    }
-
     const category = await this.categoriesRepository.create({ name, emoji });
 
     return category;
