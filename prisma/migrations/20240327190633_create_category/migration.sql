751dbb341513d3acf43ceddd9b57b47454224813
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-03-27 16:07:46 -0300

    feat: create model category

---
+++ prisma/migrations/20240327190633_create_category/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "emoji" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
