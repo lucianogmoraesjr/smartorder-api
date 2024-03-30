e585b7d3a77fa9a79c397d01d6e45040df850c7f
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-03-30 12:07:31 -0300

    feat: create prisma order model

--- src/repositories/products-ingredients-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from '@prisma/client';
-
-export interface IProductsIngredientsRepository {
-  create(data: Prisma.ProductsIngredientsUncheckedCreateInput): Promise<void>;
-  createMany(
-    data: Prisma.ProductsIngredientsUncheckedCreateInput[],
-  ): Promise<void>;
-}
