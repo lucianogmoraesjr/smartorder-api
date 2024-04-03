28621ef6f7697949b454dc49b083ad6db38265e3
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-03 17:47:45 -0300

    feat: api docs with swagger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import path from 'node:path';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import { Server } from 'socket.io';
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerDocument from '../swagger.json';
 
 import { AppError } from './errors/app-error';
 import { router } from './router';
@@ -16,6 +19,9 @@ export const io = new Server(server);
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use(router);
 app.use('/tmp', express.static(path.resolve(__dirname, '..', 'tmp')));
 
