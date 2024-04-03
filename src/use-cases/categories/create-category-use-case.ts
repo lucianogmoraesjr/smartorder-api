49d478ab7703400bf55460ed8204b0586be668bf
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-02 23:02:33 -0300

    feat: validate if category already exists before create new category

--- src/use-cases/categories/create-category-use-case.ts
+++ src/use-cases/categories/create-category-use-case.ts
@@ -1,10 +1,19 @@
 import { ICreateCategoryDTO } from '../../dtos/create-category-dto';
 import { ICategoriesRepository } from '../../repositories/categories-repository';
 
+import { AppError } from '@/errors/app-error';
+
 export class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   async execute({ name, emoji }: ICreateCategoryDTO) {
+    const categoryAlreadyExists =
+      await this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new AppError('Category already exists.', 400);
+    }
+
     const category = await this.categoriesRepository.create({ name, emoji });
 
     return category;
