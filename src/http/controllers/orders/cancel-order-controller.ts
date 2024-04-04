3b6e381f822d66857dadd2488244fb07122392fc
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-04 18:56:53 -0300

    feat: validate cancel order request

--- src/http/controllers/orders/cancel-order-controller.ts
+++ src/http/controllers/orders/cancel-order-controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
+import z from 'zod';
 
 import { makeCancelOrderUseCase } from '@/use-cases/orders/factories/make-cancel-order-use-case';
 
 export class CancelOrderController {
   async handle(request: Request, response: Response) {
-    const { orderId } = request.params;
+    const cancelOrderParamsSchema = z.object({
+      orderId: z.string().cuid().or(z.string().uuid()),
+    });
+
+    const { orderId } = cancelOrderParamsSchema.parse(request.params);
 
     const cancelOrderUseCase = makeCancelOrderUseCase();
 
