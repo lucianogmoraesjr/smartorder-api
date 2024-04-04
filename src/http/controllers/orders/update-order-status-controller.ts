1c6d30d8556ce9936f10fc8f15687717f6d87aa7
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-04 19:02:55 -0300

    feat: validate create order request

--- src/http/controllers/orders/update-order-status-controller.ts
+++ src/http/controllers/orders/update-order-status-controller.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
+import z from 'zod';
 
 import { makeUpdateOrderStatusUseCase } from '@/use-cases/orders/factories/make-update-order-status-use-case';
 
 export class UpdateOrderStatusController {
   async handle(request: Request, response: Response) {
-    const { status } = request.body;
-    const { orderId } = request.params;
+    const updateOrderStatusParamsSchema = z.object({
+      orderId: z.string().cuid().or(z.string().uuid()),
+    });
+
+    const updateOrderStatusBodySchema = z.object({
+      status: z.enum(['WAITING', 'IN_PRODUCTION', 'DONE']),
+    });
+
+    const { status } = updateOrderStatusBodySchema.parse(request.body);
+    const { orderId } = updateOrderStatusParamsSchema.parse(request.params);
 
     const updateOrderStatusUseCase = makeUpdateOrderStatusUseCase();
 
