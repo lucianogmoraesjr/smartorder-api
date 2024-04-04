e9969a88a90c97b010038034d56ae0fdf32c504c
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-04 19:02:55 -0300

    feat: validate create order request

--- src/http/controllers/orders/create-order-controller.ts
+++ src/http/controllers/orders/create-order-controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express';
+import z from 'zod';
 
 import { makeCreateOrderUseCase } from '@/use-cases/orders/factories/make-create-order-use-case';
 
 export class CreateOrderController {
   async handle(request: Request, response: Response) {
-    const { table, products } = request.body;
+    const createOrderBodySchema = z.object({
+      table: z.number(),
+      products: z.array(
+        z.object({
+          productId: z.string().cuid().or(z.string().uuid()),
+          quantity: z.number().min(1),
+        }),
+      ),
+    });
+
+    const { table, products } = createOrderBodySchema.parse(request.body);
 
     const createOrderUseCase = makeCreateOrderUseCase();
 
