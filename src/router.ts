3b30b4c63541a26d4fb13d5ed06fcca11dd36dae
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-04-16 18:59:57 -0300

    feat: ensure authenticated and admin

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import { ordersRoutes } from './http/controllers/orders/routes';
 import { productsRoutes } from './http/controllers/products/routes';
 import { AuthenticateController } from './http/controllers/users/authenticate-controller';
 import { usersRoutes } from './http/controllers/users/routes';
+import { ensureAuthenticated } from './middlewares/ensure-authenticated';
 
 export const router = Router();
 
@@ -13,6 +14,7 @@ const authenticateController = new AuthenticateController();
 
 router.use('/authenticate', authenticateController.handle);
 
+router.use(ensureAuthenticated);
 router.use('/users', usersRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/orders', ordersRoutes);
